09ff24d7713cb962a82473eeae6cdcedfc4a4b12
vict0rcarvalh0 <victordecarvalho342@gmail.com>
2024-12-09 12:50:23 -0300

    feat: function calls in lib

--- programs/nft-staking/src/lib.rs
+++ programs/nft-staking/src/lib.rs
@@ -14,7 +14,23 @@ declare_id!("HTZ275L8mFESgZSXo4b3EJAp45fgchDkrZEw8fY2UK8x");
 pub mod nft_staking {
     use super::*;
 
-    // pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-    //     Ok(())
-    // }
+    pub fn initialize_config(ctx: Context<InitializeConfig>, points_per_stake: u8, max_stake: u8, freeze_period: u32) -> Result<()> {
+        ctx.accounts.initialize_config(points_per_stake, max_stake, freeze_period, &ctx.bumps)
+    }
+
+    pub fn initialize_user(ctx: Context<Initialize>) -> Result<()> {
+        ctx.accounts.initialize_user(&ctx.bumps)
+    }
+
+    pub fn stake(ctx: Context<Stake>) -> Result<()> {
+        ctx.accounts.stake(&ctx.bumps)
+    }
+
+    pub fn unstake(ctx: Context<Unstake>) -> Result<()> {
+        ctx.accounts.unstake()
+    }
+
+    pub fn claim_rewards(ctx: Context<Claim>) -> Result<()> {
+        ctx.accounts.claim()
+    }
 }
